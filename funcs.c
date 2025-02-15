018a03a57934aaee9b54958bcf58c50f062d872b
Y-ME-tho <f20240969@pilani.bits-pilani.ac.in>
2025-02-15 13:45:13 +0530

    1

---
+++ funcs.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int addition(int, int);
+
+int main(){
+    int a,b,c;
+    scanf("%d %d", &a, &b);
+    printf("%d", addition(a,b));
+}
+
+int addition(int a, int b){
+    return a+b;
+}
